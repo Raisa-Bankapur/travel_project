5bec336a8259e50bc8d17c0a0d8b4e75b78ef1a6
Raisa-Bankapur <raisabankapur123@gmail.com>
2026-04-26 20:54:10 +0530

    added backend url

--- travel-buddy-frontend/app/login/page.tsx
+++ travel-buddy-frontend/app/login/page.tsx
@@ -16,7 +16,7 @@ export default function Login() {
     setMessage("");
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/login", {
+      const res = await fetch("https://travel-project-3-yqss.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
